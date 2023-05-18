dbd3a46f3573266ce7009eb60bcf157e83bcfa64
itlackey <itlackey@outlook.com>
2023-05-17 21:57:07 -0500

    random.org and character list

--- src/lib/components/DiceRoller.svelte
+++ src/lib/components/DiceRoller.svelte
@@ -4,28 +4,42 @@
 	import { afterUpdate, onDestroy } from 'svelte';
 	import { fade } from 'svelte/transition';
 
+	const diceList = ['⚀', '⚁', '⚂', '⚃', '⚄', '⚅']; // Dice characters
 	export let autoConfirm = false;
 	export let rollDuration = 500; // Duration of rolling animation in milliseconds
+	export let characterList = [...diceList, ...generateRandomCharacters()];
 
 	let rolling = false;
 	let characters = [];
-    let character = '';
+	let character = '';
 	let finalNumber = null;
 	let rollTimer = null;
-	const characterList = ['⚀', '⚁', '⚂', '⚃', '⚄', '⚅']; // Dice characters
+
+	function generateRandomCharacters() {
+		const characterList = [];
+		const numCharacters = 50;
+
+		for (let i = 0; i < numCharacters; i++) {
+			const randomCodePoint = Math.floor(Math.random() * (0xffff - 0x20) + 0x20);
+			const randomCharacter = String.fromCodePoint(randomCodePoint);
+			characterList.push(randomCharacter);
+		}
+
+		return characterList;
+	}
 
 	export async function rollDice() {
 		rolling = true;
 		characters = [];
 
-		const rollInterval = 10; // Interval between character changes in milliseconds
+		const rollInterval = rollDuration / 15; // Interval between character changes in milliseconds
 
 		rollTimer = setInterval(() => {
 			// characters = Array.from(
 			// 	{ length: 3 },
 			// 	() => characterList[Math.floor(Math.random() * characterList.length)]
 			// );
-            character = characterList[Math.floor(Math.random() * characterList.length)];
+			character = characterList[Math.floor(Math.random() * characterList.length)];
 		}, rollInterval);
 
 		finalNumber = await generateNumber();
@@ -74,10 +88,22 @@
 
 		// const data = await response.json();
 		// return data.result.random.data[0];
+		return fetch(
+			'https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new'
+		)
+			.then((response) => response.text())
+			.then((data) => {
+				const randomNumber = parseInt(data, 10);
+				console.log(randomNumber);
+				return randomNumber;
+			})
+			.catch((error) => {
+				console.error('Error fetching random number:', error);
+			});
 
-		return new Promise((resolve) => {
-			setTimeout(() => resolve(getRandomNumber()), rollDuration);
-		});
+		// return new Promise((resolve) => {
+		// 	setTimeout(() => resolve(getRandomNumber()), rollDuration);
+		// });
 	}
 
 	function getRandomNumber() {
@@ -93,12 +119,12 @@
 		<div class="dice-roller" on:click={rollDice} on:keydown={rollDice}>
 			{#if rolling}
 				<!-- {#each characters as character, i} key={i}-->
-					<span  in:fade>
-						{character}
-					</span>
+				<span in:fade>
+					{character}
+				</span>
 				<!-- {/each} -->
 			{:else if finalNumber > 0}
-				{characterList[finalNumber - 1]}
+				{diceList[finalNumber - 1]}
 			{/if}
 		</div>
 	</div>
@@ -106,7 +132,7 @@
 		{#if !rolling && finalNumber > 0}
 			<button on:click={confirmRoll}>confirm</button>
 		{:else}
-			<button on:click={rollDice}>roll</button>
+			<button on:click={rollDice} disabled={rolling}>roll</button>
 		{/if}
 	</div>
 </div>
