282c85d9a084e60d9e9eae77b3be27153c7567e7
itlackey <itlackey@outlook.com>
2023-06-26 23:41:58 -0500

    updated exports

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,5 @@
 // Reexport your entry components here
 export { default as Game } from './components/Game.svelte'
-// export { gameStore, currentScreen } from './stores/WAAStore.js'
+export { default as GameSelector } from './components/GameSelector.svelte'
+export { gameStore, currentScreen } from './stores/WAAStore.js'
+export { SystemSettings } from './configuration/SystemSettings.js'
